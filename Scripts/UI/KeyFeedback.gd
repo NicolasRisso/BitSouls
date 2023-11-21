ee8e7d68f868e7ab7b73569372d0d1a490b11f3b
Nicolas Risso <risso.nicolas@usp.br>
2023-11-21 17:17:16 -0300

    KeyFeedback pt2
    >Fixed bug
    > Added multi box detection at once

--- Scripts/UI/KeyFeedback.gd
+++ Scripts/UI/KeyFeedback.gd
@@ -1,8 +1,16 @@
 extends AnimatedSprite
 
+var areaCount = 0
+
 func _ready():
 	Signals.connect("interactArea", self, "setAnim")
 	visible = false
 	
 func setAnim(value):
-	visible = value
+	print(areaCount)
+	if (value): areaCount += 1
+	else: areaCount -= 1
+	if areaCount > 1:
+		visible = true
+	else:
+		visible = false
